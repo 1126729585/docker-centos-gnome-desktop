c522dc14ecf6a20d219f805685fa92fc0ffbcff1
Grant Williamson <traxtopel@gmail.com>
2014-05-09 13:44:20 +0200

    Third commit

--- startup.sh
+++ startup.sh
@@ -12,5 +12,8 @@ chown -R centos.centos /home/centos
 dbus-uuidgen > /var/lib/dbus/machine-id
 # start services
 /usr/bin/supervisord -c /supervisord.conf
+# fix suid
+chmod +s /usr/bin/sudo
+chmod +s /usr/bin/consolehelper
 # open a shell
 /bin/bash
